dbd21ff3e08ce155fa15354abd70eb44386c4ef3
Ginto11 <salinitosnelson@gmail.com>
2025-06-23 22:09:58 -0500

    Añadiendo nuevas funcionalidas

---
+++ Controllers/GeneroController.cs
@@ -0,0 +1,37 @@
+﻿using GestionUniversidad.Dtos.Genero;
+using GestionUniversidad.Services;
+using GestionUniversidad.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionUniversidad.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = "Administrador")]
+    [Route("api/generos")]
+    public class GeneroController : Controller
+    {
+        private readonly GeneroService generoService;
+
+        public GeneroController(GeneroService generoService)
+        {
+            this.generoService = generoService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetGeneroDto>>> ListarGeneros()
+        {
+            try
+            {
+                var generosDto = await generoService.FindAll();
+                return Ok(generosDto);
+            }
+            catch (Exception error)
+            {
+                return ManejoRespuestas.ServerError(error.Message);
+            }
+        }
+
+    }
+}
---
+++ Services/GeneroService.cs
@@ -0,0 +1,36 @@
+﻿using GestionUniversidad.Db;
+using GestionUniversidad.Dtos.Genero;
+using GestionUniversidad.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionUniversidad.Services
+{
+    public class GeneroService
+    {
+        private readonly Database context;
+
+        public GeneroService(Database context)
+        {
+            this.context = context;
+        }
+
+
+        public async Task<IEnumerable<GetGeneroDto>> FindAll()
+        {
+            try
+            {
+                return await context.Genero
+                    .Select(genero => new GetGeneroDto
+                    {
+                        Id = genero.Id,
+                        Nombre = genero.Nombre
+                    }).ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
