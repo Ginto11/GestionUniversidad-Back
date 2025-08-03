c40f028588d1936591527aec77347a62782491bd
Ginto11 <salinitosnelson@gmail.com>
2025-08-03 16:48:36 -0500

    Añadiendo secretos de usuarios

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using GestionUniversidad.Interfaces;
+using GestionUniversidad.Models;
+using GestionUniversidad.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionUniversidad.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService emailService;
+        public EmailController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Enviar(MensajeEmail mensajeEmail)
+        {
+            try
+            {
+                await emailService.SendEmailFromAplication(mensajeEmail);
+                return Ok();
+            }
+            catch(Exception error)
+            {
+                return ManejoRespuestas.ServerError(error.Message);
+            }
+        }
+    }
+}
